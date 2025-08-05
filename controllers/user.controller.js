619a96c7804e2e1540fede74ae2c61906d80c791
swapnilbadave49 <swapnilbadave007@gmail.com>
2025-08-05 12:54:15 +0530

    toast

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -120,7 +120,7 @@ export const login = async (req, res) => {
       role: user.role,
       profile: user.profile
     }
-    return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpsOnly: true, sameSite: 'None',secure:true }).json(
+    return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'None',secure:true }).json(
       {
         message: "Login successful",
         success: true, user, token
