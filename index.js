ddc06fb91d8d58981881ea1599370b57092cebc3
swapnilbadave49 <swapnilbadave007@gmail.com>
2025-08-05 11:28:00 +0530

    fix: update CORS origin to deployed frontend

--- index.js
+++ index.js
@@ -25,11 +25,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-   
-    origin: 'http://localhost:5173', // Replace with your frontend URL
-    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
-
+  origin: [
+    'http://localhost:5173',
+    'https://job-portal-ui-jet.vercel.app',
+  ],
+  credentials: true,
 };
+
 
 app.use(cors(corsOptions));
 
